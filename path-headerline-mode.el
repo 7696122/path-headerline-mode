1e4b1844e3f2846e0d48d5167322a7e0f39ecc48
7696122 <7696122@simplucid.net>
2012-09-08 11:45:07 +0900

    make header

--- path-headerline-mode.el
+++ path-headerline-mode.el
@@ -1,4 +1,53 @@
+;;; path-headerline-mode.el ---
+;;
+;; Filename: path-headerline-mode.el
+;; Description:
+;; Author: 7696122
+;; Maintainer:
+;; Created: Sat Sep  8 11:44:11 2012 (+0900)
+;; Version: 0.1
+;; Last-Updated: Sat Sep  8 11:44:33 2012 (+0900)
+;;           By: 7696122
+;;     Update #: 1
+;; URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
+
 (defmacro with-face (str &rest properties)
   `(propertize ,str 'face (list ,@properties)))
 
@@ -53,3 +102,6 @@
           'ph/display-header)
 
 (provide 'path-headerline-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; path-headerline-mode.el ends here
